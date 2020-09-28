c85133268b281d1dc37b57741a0b977def1f704a
alifrazanit <alifrazanit@gmail.com>
2020-09-28 21:27:38 +0700

    ifstatement

---
+++ WEEK 5/LATIHAN_2/fundamental_javascript/js/basicOperators.js
@@ -0,0 +1,37 @@
+// source : https://javascript.info/operators
+// Addition +,
+// Subtraction -,
+// Multiplication *,
+// Division /,
+// Remainder %,
+// Exponentiation **.
+
+
+alert( 5 % 2 );
+alert( 2 ** 2 );
+alert( 2 ** 3 );
+alert( 2 ** 4 );
+
+//penyatuan gitulah
+let s = "my" + "string";
+alert(s);
+alert( 2 + '1' ); // "21"
+
+alert( 6 - '2' ); // 4, converts '2' to a number
+alert( '6' / '2' ); // 3, converts both operands to numbers
+
+let x = 1; 
+alert( +x ); // 1
+
+let y = -2;
+alert( +y ); 
+
+let z = 2 * 2 + 1;
+alert(z);
+
+let increment = 1;
+alert(++increment);
+
+let decrement = 1;
+alert(--decrement);
+
---
+++ WEEK 5/LATIHAN_2/fundamental_javascript/js/ifstatement.js
@@ -0,0 +1,3 @@
+let todayisrain = false;
+let res = todayisrain ? 'Pakai Payung' : 'tidak pakai payung'; //one line if statement
+alert(res);
